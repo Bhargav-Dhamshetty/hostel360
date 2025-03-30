64a5d6023af96b071effb3c9d306c275b1c6b709
Bhargav-Dhamshetty <bhargavdhamshetty@gmail.com>
2025-03-30 12:27:01 +0530

    final  commit

--- backend/APIs/userApi.js
+++ backend/APIs/userApi.js
@@ -7,7 +7,7 @@ const requireAuth = require("../middleware/clerkAuth"); // Ensure correct export
 const userApp = express.Router();
 
 // ✅ Enable CORS
-userApp.use(cors({ origin: "http://localhost:5173" , credentials: true }));
+userApp.use(cors({ origin: "https://hostel360-rosy.vercel.app" , credentials: true }));
 
 // ✅ Middleware to Parse JSON
 userApp.use(express.json());
--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 1800;
 
 // CORS Configuration
 const corsOptions = {
-  origin: [ "http://localhost:5173"], // Allowed frontend domains
+  origin: [ "https://hostel360-rosy.vercel.app"], // Allowed frontend domains
   methods: ["GET", "POST", "PUT", "DELETE","PATCH"], // Allowed HTTP methods
   allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
   credentials: true, // Allow cookies & credentials (useful for authentication)
